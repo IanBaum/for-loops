8b7a295f158c71f7f0d295ef0f9c8a613bbf80ee
Ian Baum and Zach Hedgepeth <zach@>
2016-08-16 11:43:59 -0400

    add word puzzle factorials and palindrome html and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // Counter
   $("#userNumbers").submit(function(event) {
     event.preventDefault();
     var countTo = parseInt($('#countTo').val());
@@ -15,9 +16,71 @@ $(document).ready(function(){
     }
     else {
       $("#output").text("");
-      for(var index = countBy; index <= countTo; index += countBy)
+      for(var index = countBy; index <= countTo; index += countBy){
         $('#output').append(" " + index);
-    }    
+      }
+    }
+  });
+  // Word Puzzle
+  $("#puzzleInfo").submit(function(event){
+    event.preventDefault();
+
+    var puzzleString = $("#puzzleString").val();
+    var puzzleArray = puzzleString.split("");
+
+    for(var index = 0; index < puzzleArray.length; index++){
+      if(puzzleArray[index] === "a" ||puzzleArray[index] === "e" ||puzzleArray[index] === "i" ||puzzleArray[index] === "o" ||puzzleArray[index] === "u"){
+        puzzleArray[index] = "-";
+      }
+    }
+    $("#puzzleOutput").text(puzzleArray.join(""));
+    $("#puzzleString").val("");
+
   });
 
+  $("#factorialInfo").submit(function(event){
+    event.preventDefault();
+
+    var number = parseInt($('#factorialNumber').val());
+    var outputNumber = 1
+
+    for(var index = number; index > 0; index--){
+      outputNumber *= index;
+    }
+    $('#factorialOutput').text(outputNumber);
+  });
+
+  $("#palindromeInfo").submit(function(event){
+    event.preventDefault();
+
+    var palindromeString = $('#palindromeString').val().toLowerCase();
+    var palindromeArray = palindromeString.split("");
+    var noSpaceArray = [];
+    var firstHalfArray = [];
+    var secondHalfArray = [];
+    console.log(palindromeArray);
+
+    for(var index = 0; index < palindromeArray.length; index++){
+      if(palindromeArray[index] != " "){
+        noSpaceArray.push(palindromeArray[index]);
+      }
+    }
+
+    for(var index = 0; index < Math.floor(noSpaceArray.length/2); index++){
+        firstHalfArray.push(noSpaceArray[index]);
+    }
+    console.log(firstHalfArray);
+    for(var index = Math.ceil(noSpaceArray.length/2); index < noSpaceArray.length; index++){
+        secondHalfArray.push(noSpaceArray[index]);
+    }
+    console.log(secondHalfArray);
+    secondHalfArray.reverse()
+    if(firstHalfArray.toString() === secondHalfArray.toString()){
+      $("#palindromeOutput").text("This is a Palindrome!")
+    }
+    else {
+      $("#palindromeOutput").text("This is not a Palindrome!")
+    }
+
+  });
 });
